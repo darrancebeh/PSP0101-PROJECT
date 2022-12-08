b3a09ff15d04183e95bf634261d00f3114fcbbbf
darrancebeh <darrancebeh@gmail.com>
2022-12-08 19:58:48 +0800

    aaa

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+# *********************************************************
+# Program: TLXV_GX.py
+# Course: PSP0101 PROBLEM SOLVING AND PROGRAM DESIGN
+# Tutorial Section: TLX Group: GX
+# Trimester: 2215
+# Year: 2022/23 Trimester 1
+# Member_1: 1211108266 | DARRANCE BEH HENG SHEK
+# Member_2: 1221101417 | AIRIL BIN AZLAN
+# Member_3: 1211107977 | HONG CHIA QIAN
+# Member_4: 1211109799 | LEE MEI SHAN
+# *********************************************************
+# Task Distribution
+# Member_1:
+# Member_2:
+# Member_3:
+# Member_4:
+# *********************************************************
+
 def first():
-    option = input("Would you like to login or register? \n")[0].lower()
+    option = input("Would you like to login or register? \n").lower()
     if option != "register" and option != "login":
         first()
     else:
@@ -7,9 +25,16 @@ def first():
 
 def second(option):
     if option == "register":
-        username = input("Input your username: ")
-        password = input("Input your password: ")
-        register(username,password)
+        username = input("Enter your desired username: ")
+        while(True):
+            password = input("Enter your desired password: ")
+            pwcheck = input("Please enter your password again: ")
+            if(password == pwcheck):
+                print(f"\nRegistration Success! Welcome, {username}!")
+                register(username,password);
+                return False;
+            else:
+                print("\nPasswords do not match.\nRedirecting back to password data entry...\n")
     else:
         username = input("Enter your username: ")
         password = input("Enter your password: ")
@@ -37,7 +62,4 @@ def app():
     print("Welcome to Darrance's Login System!")
     print("Success.")
 
-
-
-
-dglkffsjdfjdfoidsjfiosfirst()
+first()
