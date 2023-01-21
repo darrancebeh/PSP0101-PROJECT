2327a6ddb24b73d5ff998a345c3803da17dd7076
darrancebeh <darrancebeh@gmail.com>
2023-01-22 02:23:48 +0800

    ohmygoddd

--- main.py
+++ main.py
@@ -27,14 +27,14 @@ def clear():
 
 #login system | login or register
 def logOrReg():
-    option = input("Would you like to login or register? \n").lower();
+    option = input("Would you like to login or register? \n\n").lower();
     return option;
 
 #login system | login/registration
 def logReg(option):
     #user registration
     if option == "register":
-        username = input("Enter your desired username: ");
+        username = input("\nEnter your desired username: ");
 
         if(isSameUsername(username)):
             print("\nSame username exists within our database. Please register with another name.")
@@ -44,46 +44,50 @@ def logReg(option):
 
         flag = True;
         while(flag):
-            password = input("Enter your desired password: ")
-            pwcheck = input("Please enter your password again: ")
+            password = input("Enter your desired password: ");
+            pwcheck = input("Please enter your password again: ");
             if(password == pwcheck):
-                return("regUser", username, password)
+                if(username.lower() == "admin"):
+                    return("regUser", username, password, "ADMIN");
+                else:
+                    return("regUser", username, password, "MEMBER");
             else:
                 rFlag = True;
                 while(rFlag):
                     opt = input("\nPasswords do not match.\nWould you like to try again? [ Y / N ]\n").lower();
                     if(opt == 'n'):
                         rFlag = False;
-                        print("Redirecting to first screen...\n")
+                        print("Redirecting to first screen...\n");
                         loginSys();
                     elif(opt == 'y'):
                         rFlag = False;
     
     #user/admin login
     elif option == "login":
-        username = input("Enter your username: ")
+        username = input("\nEnter your username: ")
         if(username == "admin"):
             print("\nAdmin Account Login Attempt Detected...");
             admintest = input("Please Enter the Administrator Password: ");
-            return("logUser", username, admintest);
+            return("logUser", username, admintest, "ADMIN");
         else:
             password = input("Enter your password: ")
-            return("logUser", username, password)
+            return("logUser", username, password, "MEMBER");
 
 #login system | registration
-def register(username,password):
+def register(username, password, status):
     file = open("registry.txt", "a");
     
-    if(username == "admin"):
-        file.write(f"{username} | {password} | ADMIN\n");
-        print(f"Administrator Registration Success! Welcome, {username}!");
+    file.write(f"{username} | {password} | {status}\n");
+
+    if(status == "ADMIN"):
+        print(f"\nAdministrator Registration Success! Welcome, {username}!");
     else:
-        file.write(f"{username} | {password} | MEMBER\n");
         print(f"\nMember Registration Success! Welcome, {username}!");
     
     file.close();
 
     print("Redirecting to first screen...\n");
+    time.sleep(1);
     loginSys();
     
 #login system | check for username duplicate
@@ -99,7 +103,7 @@ def isSameUsername(username):
     return False;
 
 #login system | login
-def login(username,password):
+def login(username, password, status):
     flag = True;
     file = open("registry.txt", "r")
     for info in file:
@@ -110,12 +114,12 @@ def login(username,password):
         if(username == a and password == b):
             if(c == "ADMIN"):
                 print("Admin Login Attempt Successful! Navigating to Administrative Menu...\n");
-                admin(username, password);
+                admin(username, password, status);
                 flag = False;
 
-            elif(c == "USER"):
+            elif(c == "MEMBER"):
                 print(f"Login Successful! Welcome {username}!");
-                app(username, password);
+                app(username, password, status);
                 flag = False;
 
             elif(c == "BANNED"):
@@ -235,7 +239,76 @@ def loginChangePassword(username, password):
                 print("Invalid input. Please try again!\n");
 
 def viewStore(username, password):
-    pass;
+    clear();
+    print("ADHL Fashion Store Catalogue: \n");
+    file = open("storeMenu.txt", "r");
+
+    for info in file:
+        a, b, c = info.split(" | ");
+        a = a.strip();
+        b = b.strip();
+        c = c.strip();
+
+        print(f"{a} | Item: {b} | Price: RM{c}");
+    file.close();
+
+    option = input("\n\nWhat Would You Like to Do?\n\n[1] - Add Items To Cart\n[2] - View Cart\n[3] - Return to User Menu\n\n")
+
+    funcDict = {
+        '2' : viewCart,
+        '3' : app,
+    };
+
+    if(option == '1'):
+        clear();
+        print("ADHL Fashion Store Catalogue:\n");
+        flag = True;
+        totalCart = [];
+
+        file = open("storeMenu.txt", "r");
+
+        for info in file:
+            a, b, c = info.split(" | ");
+            a = a.strip();
+            b = b.strip();
+            c = c.strip();
+
+            print(f"{a} | Item: {b} | Price: RM{c}");
+
+        while(True):
+            aTCart = input("\n\nEnter The Item's Respective Number to Add Item to Cart: ");
+            totalCart += aTCart;
+            opt = input("Do You Want to Add More Items To Cart? [ Y / N ]\n").lower();
+            
+            if(opt != 'y' and opt != 'n'):
+                while(flag):
+                    print("Please Input a Valid Option");
+                    opt = input("Do You Want to Add More Items To Cart? [ Y / N ]\n");
+                    if(opt == 'y' or opt == 'n'):
+                        flag = False;
+            else:
+                if(opt == 'y'):
+                    pass;
+                else:
+                    addCart(username, password, totalCart);
+                    break;
+
+    else:
+        funcDict[option](username, password);
+
+## CONTINUE HERE
+## CONTINUE HERE 
+## CONTINUE HERE
+## CONTINUE HERE 
+## CONTINUE HERE
+## CONTINUE HERE 
+## CONTINUE HERE
+## CONTINUE HERE V
+# YOU STOPPED HERE!!!!
+def addCart(username, password, cartItems):
+    print(cartItems);
+
+
 
 def viewCart(username, password):
     pass;
@@ -332,13 +405,56 @@ def adminBanMember(username, password, member):
 def adminEditMenu(username, password):
     pass;
 
+def adminManageAdmins(username, password):
+    clear();
+    print("Selected Option: New Administrator Account Creation\n");
+    print("_____________________________________________________________\n");
+    print("Registered Administrator Accounts:\n");
+
+    file = open("registry.txt", "r");
+    count = 1;
+
+    for info in file:
+        a, b, c = info.split(" | ");
+        b = b.strip();
+        c = c.strip();
+
+        if(c == "ADMIN"):
+            print(f"{count} | {a} | {b} | Status: {c}");
+
+    file.close();
+    
+    option = input("What Would You Like to Do?\n\n[1] - Create New Administrator Account\n[2] - Remove Administrator Account\n[3] - Return to Administrative Menu\n\n");
+
+    funcDict = {
+        '1' : register,
+        '2' : adminRemoveMember,
+        '3' : admin,
+    };
+
+    if(option == '1'):
+        newUsername = input("\nEnter Username for New Account: ");
+
+        flag = True;
+        while(flag):
+            newPw = input("Enter your desired password: ");
+            newPwCheck = input("Please enter your password again: ");
+            if(newPwCheck == newPw):
+                flag = False;
+                register(newUsername, newPw, "ADMIN");
+            else:
+                print("Passwords Do Not Match. Please Try Again...\n");
+    
+    else:
+        funcDict[option](username, password, "ADMIN");
+
 #admin END
 
 #MASTER FUNCTIONS START
 
 #LOGIN SYSTEM MASTER FUNCTION
 def loginSys():
-    print("_____________________________________________________________\n")
+    print("_____________________________________________________________\n");
     option = "";
     while(option != "register" or option != "login"):
         option = logOrReg();
@@ -346,7 +462,7 @@ def loginSys():
             break;
         else:
             print("Please Enter a Valid Input. [ Login / Register ]\n");
-    fType, username, password = logReg(option);
+    fType, username, password, status = logReg(option);
 
     funcDict = {
         "admin" : admin,
@@ -354,10 +470,10 @@ def loginSys():
         "logUser" : login,
     }
     
-    funcDict[fType](username, password);
+    funcDict[fType](username, password, status);
 
 #USER APP MASTER FUNCTION
-def app(username, password):
+def app(username, password, status):
     clear();
     print("_____________________________________________\n")
     print("Welcome to ADHL Fashion Boutique!");
@@ -380,21 +496,22 @@ def app(username, password):
 
 
 #ADMIN APP MASTER FUNCTION
-def admin(username, password):
+def admin(username, password, status):
     clear();
     print(f"Welcome back, {username}!");
     print("\nAdministrative Menu");
     print(f"What would you like to do, {username}?\n");
 
-    option = input("[1] - Change Administrator Password\n[2] - Manage Registered Member List\n[3] - Edit Store Menu\n[4] - Return to Login Menu\n\n");
+    option = input("[1] - Change Administrator Password\n[2] - Manage Registered Member List\n[3] - Manage Administrator Accounts\n[4] - Edit Store Menu\n[5] - Return to Login Menu\n\n");
 
     appDict = {
         '1' : loginChangePassword,
         '2' : adminManageMember,
-        '3' : adminEditMenu,
+        '3' : adminManageAdmins,
+        '4' : adminEditMenu,
     };
 
-    if(option == '4'):
+    if(option == '5'):
         loginSys();
     else:
         appDict[option](username, password);
